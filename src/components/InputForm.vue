d59e57e8824c9f1b594392ce97b859a64c8b7175
tanq-design <areatank@areatank.com>
2024-06-20 13:33:57 +0900

    入力機能搭載

---
+++ src/components/InputForm.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = defineModel('title');
+const content = defineModel('content');
+const message = ref("入力してください");
+
+async function submitData() {
+    const jsonbody = { title: title.value, content: content.value };
+
+    const res = await fetch('v1/memo', {
+        method: 'POST',
+        body: JSON.stringify(jsonbody)
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error, status: ${response.status}, text: ${response.statusText}`);
+            }
+            message.value = "記録しました";
+            title.value = "";
+            content.value = "";
+        })
+        .catch((error) => {
+            message.value = `記録に失敗しました<${error}>`;
+        });
+}
+</script>
+
+<template>
+    <form>
+        <label>タイトル
+            <input v-model="title" type="text" placeholder="TITLE">
+        </label>
+        <label>内容
+            <textarea v-model="content" placeholder="CONTENT"></textarea>
+        </label>
+        <button type="button" @click="submitData">Submit</button>
+    </form>
+    <div>
+        {{ message }}
+    </div>
+</template>
--- src/pages/v1/memo.ts
+++ src/pages/v1/memo.ts
@@ -1,20 +1,59 @@
 import { drizzle } from 'drizzle-orm/d1';
 import { memos } from "../../db/schema";
 import type { APIContext } from 'astro';
+import type { Runtime } from '@astrojs/cloudflare';
 
-export async function GET({ locals }: APIContext) {
-    const db = drizzle(locals.runtime.env.DB as D1Database);
+interface PostData {
+    title: string
+    content: string
+}
+
+export async function GET({ locals }: { locals: Runtime }) {
+    const envDB = locals.runtime.env.DB as D1Database
+    const db = drizzle(envDB);
     let memo = await db.select().from(memos);
-    
-    console.log(memo);
-    if(!memo || (Array.isArray(memo) && memo.length === 0)) {
-       memo = [{id:1, title:"DUMMY1", content:"content1"},{id:2, title:"DUMMY2", content:"content2"}] 
+
+    if (!memo || (Array.isArray(memo) && memo.length === 0)) {
+        memo = [{ id: 1, title: "DUMMY1", content: "content1" }, { id: 2, title: "DUMMY2", content: "content2" }]
     }
 
     return new Response(
         JSON.stringify({
             body: { memos: memo }
-        })
-    )
+        }), { status: 200 });
 }
 
+export async function POST({ request, locals }: { request: Request, locals: Runtime }) {
+    const message: PostData = await request.json();
+
+    if (!message) {
+        console.log("POST data is empty!");
+        return new Response(null, {
+            status: 500,
+        });
+    }
+
+    if (!message.hasOwnProperty('title') || !message.hasOwnProperty('content')) {
+        console.log("POST data is invalid!");
+        return new Response(null, {
+            status: 500,
+        });
+    }
+    const title = message.title;
+    const content = message.content;
+    const envDB = locals.runtime.env.DB as D1Database;
+
+    const db = drizzle(envDB);
+    const result = await db.insert(memos).values({ title, content });
+
+    if (!result.success) {
+        console.log("DB insert failed!");
+        return new Response(null, {
+            status: 500,
+        });
+    }
+
+    return new Response(null, {
+        status: 200,
+    });
+}
