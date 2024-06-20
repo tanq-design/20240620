5788dda722993fea59f2ef67a2c00e1f3e012fba
tanq-design <areatank@areatank.com>
2024-06-20 10:32:34 +0900

    ダミーデータ挿入

--- src/pages/v1/memo.ts
+++ src/pages/v1/memo.ts
@@ -4,9 +4,12 @@ import type { APIContext } from 'astro';
 
 export async function GET({ locals }: APIContext) {
     const db = drizzle(locals.runtime.env.DB as D1Database);
-    const memo = await db.select().from(memos);
+    let memo = await db.select().from(memos);
     
     console.log(memo);
+    if(!memo || (Array.isArray(memo) && memo.length === 0)) {
+       memo = [{id:1, title:"DUMMY1", content:"content1"},{id:2, title:"DUMMY2", content:"content2"}] 
+    }
 
     return new Response(
         JSON.stringify({
@@ -14,3 +17,4 @@ export async function GET({ locals }: APIContext) {
         })
     )
 }
+
