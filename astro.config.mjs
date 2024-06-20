0ca69482e73cfb43511a7d0be30087b3e843062b
tanq-design <areatank@areatank.com>
2024-06-20 15:21:28 +0900

    auth-astro削除3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import cloudflare from "@astrojs/cloudflare";
 
-import auth from "auth-astro";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), auth()],
+  integrations: [vue()],
   output: "server",
   adapter: cloudflare({
     platformProxy: {
